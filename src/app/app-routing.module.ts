ec555cb5111bbe0e939f8dbf462d1552195030f2
ideveshchoudhary <deveshch2@gmail.com>
2024-04-02 09:05:44 +0530

    created some component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./admin/cart/cart.module').then(m => m.CartModule)
+  },
+  // {
+  //   path: 'category',
+  //   loadChildren: () => import('./admin/category/category.module').then(m => m.CategoryModule)
+  // }
+  {
+    path: 'login',
+    loadChildren: () => import('./admin/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./admin/order/order.module').then(m => m.OrderModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./admin/products/products.module').then(m => m.ProductsModule)
   }
 ];
 
